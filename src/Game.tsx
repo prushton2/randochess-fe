b45ecbcf0b240c8a50f86f5292c583a0a02f294b
prushton2 <peterrushton418@gmail.com>
2024-05-14 00:35:04 -0400

    added proper rendering of pieces

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,28 +6,31 @@ import './Game.css'
 
 
 function RenderSquare(piece: int, number: int) {
-	
-	let column = number + parseInt(number/8);
+	let pieces: string[] = [" ", "P", "R", "N", "B", "Q", "K"];
+	let color: string = piece > 128 ? "blacktext" : "whitetext";
 
+	if(piece >= 128) {
+		piece -= 128;
+	}
+
+	let column = number + parseInt(number/8);
 	let classname: string = column%2==0 ? "lightSquare" : "darkSquare";
 
-	return <div className={classname} key={number}>
-		
-
+	return <div className={classname} key={"Board Element "+number}>
+		<label className={color}>{pieces[piece]}</label>
 	</div>
 }
 
 
 function RenderGame() {
 	const [squareHTML, setSquareHTML] = useState<JSX.Element[]>();
-	let array: int[] = [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0,129,0,0,1,0,0,0,0];
-
+	let array: int[] = [130,131,132,133,134,132,131,130,129,129,129,129,129,129,129,129,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,2,3,4,5,6,4,3,2];
 	useEffect(() => {
 		let squares: JSX.Element[] = []
 
 		for(let i: int = 0; i < 64; i++) {
 			if(i%8 == 0) {
-				squares.push(<br />);
+				squares.push(<br key={"break "+i}/>);
 			}
 			squares.push(RenderSquare(array[i], i));
 		}
