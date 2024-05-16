f7f60e4280621d745a93bbf89b708cb1bd4f27ff
prushton2 <peterrushton418@gmail.com>
2024-05-16 16:18:41 -0400

    added multi player refreshing

--- src/Game.tsx
+++ src/Game.tsx
@@ -12,7 +12,8 @@ function RenderGame() {
 	const [squareHTML, setSquareHTML] = useState<JSX.Element[]>();
 	const [start_pos, setStart_pos] = useState<int>(-1);
 	const [end_pos, setEnd_pos] = useState<int>(-1);
-	
+	const [run_update, setRun_update] = useState<int>(1); 
+	const [winner, setWinner] = useState<string>("");
 	let [query] = useSearchParams();
 
 	function manageClick(number: int) {
@@ -43,8 +44,9 @@ function RenderGame() {
 		let column = number + parseInt(number/8);
 		let classname: string = column%2==0 ? "square light" : "square dark";
 
-		if(start_pos == number) { classname = "square red" }
-		if(end_pos == number)   { classname = "square red" }
+		if(start_pos == number) { console.log("1 red"); classname = "square red" }
+		if(end_pos == number)   { console.log("2 red"); classname = "square red" }
+
 
 		return <div className={classname} key={"Board Element "+number} onClick={(e) => {manageClick(number)}}>
 			<label className={color}>{pieces[piece]}</label>
@@ -52,33 +54,54 @@ function RenderGame() {
 	}
 	
 	async function init() {
+		if(winner != "") {
+			return;
+		}
 		if(start_pos != -1 && end_pos != -1) {
 			if ((await Move(query.get("code"), start_pos, end_pos))["status"] == "Success") {
 				setStart_pos(-1);
 				setEnd_pos(-1);
 				return;
-			} else {
-			}
+			}	
 		}
-
-		let array: int[] = (await Fetch(query.get("code")))["board"];
+		
+		let fetch = (await Fetch(query.get("code")))
+		let array: int[] = fetch["board"];
+		setWinner(fetch["winner"]);
 
 		let squares: JSX.Element[] = []
-
+		
+		
 		for(let i: int = 0; i < 64; i++) {
-			if(i%8 == 0) {
+			if(i%8 == 0 && i != 0) {
 				squares.push(<br key={"break "+i}/>);
 			}
 			squares.push(RenderSquare(array[i], i));
 		}
-
+		
+		if(fetch["black_side"] == query.get("code")) {
+			squares.reverse();
+		}
 		setSquareHTML(squares);
 
 	}
 
 	useEffect(() => {
 		init();
-	}, [start_pos, end_pos]);
+	}, [start_pos, end_pos, run_update]);
+	
+	setInterval(() => setRun_update(run_update+1), 1000);
+	
+	useEffect(() => {
+		if(winner == "") {
+			return;
+		}
+		if(winner == undefined) {
+			alert("Game does not exist");
+			return;
+		}
+		alert(winner+" has won");
+	}, [winner]);
 
 	return <>
 		{squareHTML}
@@ -90,7 +113,6 @@ function Game() {
 	let [query] = useSearchParams();
 
 	async function leaveGame() {
-		console.log("Leaving");
 		await Leave(query.get("code"));
 		window.location.href = "/";
 	}
