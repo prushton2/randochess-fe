2f161820faa14df2a33fbccd91765b84ef4b9124
prushton2 <peterrushton418@gmail.com>
2024-05-17 01:48:14 -0400

    Made the frontend a lot more stable

--- src/Game.tsx
+++ src/Game.tsx
@@ -6,16 +6,17 @@ import './Game.css'
 import { useSearchParams } from 'react-router-dom'
 
 
-
-
-function RenderGame() {
-	const [squareHTML, setSquareHTML] = useState<JSX.Element[]>();
+function Game() {
+	const [chessBoard, setChessBoard] = useState<JSX.Element[]>([]);
+	const [boardData, setBoardData] = useState<int[]>([]);
+	const [blackSide, setBlackSide] = useState<string>("");
 	const [start_pos, setStart_pos] = useState<int>(-1);
 	const [end_pos, setEnd_pos] = useState<int>(-1);
-	const [run_update, setRun_update] = useState<int>(1); 
 	const [winner, setWinner] = useState<string>("");
-	let [query] = useSearchParams();
 
+	let [query] = useSearchParams();
+	let pieces: string[] = [" ", "", "", "", "", "", ""];
+	
 	function manageClick(number: int) {
 		if(start_pos == -1) {
 			setStart_pos(number);
@@ -27,105 +28,96 @@ function RenderGame() {
 		}
 	}
 
-
-	function RenderSquare(piece: int, number: int) {
-		let pieces: string[] = [" ", "", "", "", "", "", ""];
+	function RenderSquare(i: int) {
 		let color: string = "whitetext";
-
-		if(piece >= 128) {
+		let piece: int = boardData[i];
+		if(piece > 128) {
 			piece -= 128;
 			color = "blacktext";
 		}
-
-		if (piece >= 32) {
+		if(piece >= 32) {
 			piece -= 32;
 		}
-
-		let column = number + parseInt(number/8);
+	
+		let column = i + parseInt(i/8);
 		let classname: string = column%2==0 ? "square light" : "square dark";
 
-		if(start_pos == number) { console.log("1 red"); classname = "square red" }
-		if(end_pos == number)   { console.log("2 red"); classname = "square red" }
-
-
-		return <div className={classname} key={"Board Element "+number} onClick={(e) => {manageClick(number)}}>
+		return <div className={start_pos == i || end_pos == i ? "square red" : classname} key={"board element "+i} onClick={(e) => manageClick(i)}>
 			<label className={color}>{pieces[piece]}</label>
 		</div>
 	}
-	
-	async function init() {
+
+	useEffect(() => {
 		if(winner != "") {
 			return;
 		}
-		if(start_pos != -1 && end_pos != -1) {
-			if ((await Move(query.get("code"), start_pos, end_pos))["status"] == "Success") {
-				setStart_pos(-1);
-				setEnd_pos(-1);
-				return;
-			}	
-		}
-		
-		let fetch = (await Fetch(query.get("code")))
-		let array: int[] = fetch["board"];
-		setWinner(fetch["winner"]);
-
 		let squares: JSX.Element[] = []
-		
 		
 		for(let i: int = 0; i < 64; i++) {
 			if(i%8 == 0 && i != 0) {
 				squares.push(<br key={"break "+i}/>);
 			}
-			squares.push(RenderSquare(array[i], i));
+			squares.push(RenderSquare(i));
 		}
-		
-		if(fetch["black_side"] == query.get("code")) {
+
+		if(blackSide == query.get("code")) {
 			squares.reverse();
 		}
-		setSquareHTML(squares);
 
-	}
-
-	useEffect(() => {
-		init();
-	}, [start_pos, end_pos, run_update]);
-	
-	setInterval(() => setRun_update(run_update+1), 1000);
+		setChessBoard(squares);
+	}, [boardData, start_pos, end_pos]);
 	
 	useEffect(() => {
-		if(winner == "") {
-			return;
+		async function run() {
+			if(start_pos == -1 || end_pos == -1) {
+				return;
+			}
+			if ((await Move(query.get("code"), start_pos, end_pos))["status"] == "Success") {
+
+				//do the move client side so its more responsive
+				let newBoard: int[] = boardData;
+				newBoard[end_pos] = newBoard[start_pos];
+				newBoard[start_pos] = 0;
+				setBoardData(newBoard);
+
+				setStart_pos(-1);
+				setEnd_pos(-1);
+				return;
+			}	
 		}
-		if(winner == undefined) {
-			alert("Game does not exist");
-			return;
-		}
-		alert(winner+" has won");
-	}, [winner]);
+		run();
+	}, [start_pos, end_pos])
+	
+	useEffect(() => {
+		let interval = setInterval(async() => {
+			let fetch = await Fetch(query.get("code"))
+			if(fetch["status"] != "Success") {
+				alert("Game no longer exists");
+				window.location.href = "/";
+				return;
+			}
+			setBoardData(fetch["board"]);
+			setWinner(fetch["winner"]);
+			setBlackSide(fetch["black_side"]);
+		}, 1000);
 
-	return <>
-		{squareHTML}
-	</>
-}
-
-
-function Game() {
-	let [query] = useSearchParams();
-
+		return () => clearInterval(interval);
+	}, [boardData]);
+	
 	async function leaveGame() {
 		await Leave(query.get("code"));
 		window.location.href = "/";
 	}
-  return (
-    <b>
-	<RenderGame />
-	<div className="alignBottom">
-		<button className="bottomElement red" onClick={(e) => {leaveGame()}}>Leave</button>
-		<label className="bottomElement">Join Code: {localStorage.getItem("guest_code")}</label>
-	</div>
-    </b>
-  )
+
+	return (
+	<b>
+		{chessBoard}
+		<div className="alignBottom">
+			<button className="bottomElement red" onClick={(e) => {leaveGame()}}>Leave</button>
+			<label className="bottomElement">Join Code: {localStorage.getItem("guest_code")}</label>
+		</div>
+	</b>
+	)
 }
 
-export default Game
-
+export default Game;
